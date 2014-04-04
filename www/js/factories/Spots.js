944f38a8e6263695465de25e788db22b517ea208
purplecode <niespammnie@gmail.com>
2014-04-04 10:34:41 +0200

    basic web socket transfer

--- www/js/factories/Spots.js
+++ www/js/factories/Spots.js
@@ -1,12 +1,33 @@
-angular.module('whereisdaniel-app').factory('Spots', function($http) {
+app.factory('Spots', function($http) {
+
+  var WebSocket = window.WebSocket || window.MozWebSocket;
+
+  var connection = new WebSocket('ws://localhost:3001');
+
+  connection.onopen = function() {
+    // connection is opened and ready to use
+  };
+
+  connection.onerror = function(error) {
+    // an error occurred when sending/receiving data
+  };
+
+  connection.onmessage = function(message) {
+    try {
+      var json = JSON.parse(message.data);
+    } catch (e) {
+      console.log('This doesn\'t look like a valid JSON: ', message.data);
+    }
+  };
+
   return {
 
-    url: function(url) {
-      return 'localhost:3001/' + url;
-    },
-
     addSpot: function(spot, callback) {
-      $http.post(this.url('spots'), spot).success(callback);
+      connection.send(JSON.stringify({
+        collection: 'spots',
+        action: 'add',
+        data: spot
+      }));
     },
 
     getAllSpots: function() {
--- www/js/main.js
+++ www/js/main.js
@@ -13,20 +13,4 @@ app.config(function($routeProvider) {
     templateUrl: 'views/send.html'
   });
 
-}).factory('Spots', function($http) {
-  return {
-
-    url: function(url) {
-      return 'http://localhost:3001/' + url;
-    },
-
-    addSpot: function(spot, callback) {
-      $http.post(this.url('spots'), spot).success(callback);
-    },
-
-    getAllSpots: function() {
-      return [];
-    }
-
-  };
-});;
+});
